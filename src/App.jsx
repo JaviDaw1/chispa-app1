66c2fa03f894ea94cd23a0aa7db94b97d4034995
Javier Martinez <javiermarsal11@gmail.com>
2025-04-05 15:33:58 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+export default function App() {
+  return (
+    <h1 className="text-3xl text-red-400 font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
